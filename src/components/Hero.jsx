611ee7f3e5648bbc463e96cf1a913cdd08d9769d
Vivian Zhou <87792254+vivianzhou1288@users.noreply.github.com>
2024-07-25 19:10:45 -0400

    Updated resume link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             I am a student at Cornell University <br className ="sm:block hidden" /> majoring in Computer Science.
           </p>
           <div className = "mt-5">
-            <a href="https://drive.google.com/file/d/1NWGaqyOsZ0XI2BWYXbdCqKLywkoN_zRd/view?usp=drive_link" target="_blank" rel="noopener noreferrer">
+            <a href="https://drive.google.com/file/d/121u47EKg2o_Zs3VHwtqnUMP9Dxy6crIk/view?usp=sharing" target="_blank" rel="noopener noreferrer">
               <button className = "p-3 rounded border-0 cursor-pointer">Resume</button>
             </a>
           </div>
